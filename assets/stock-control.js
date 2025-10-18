40e19fffaaa0fd073f47d717e80ac60b7bbadca7
Abdelhazemri-rgb <abdelhazemri@icloud.com>
2025-10-18 22:52:25 +0700

    test2

---
+++ assets/stock-control.js
@@ -0,0 +1,12 @@
+let variantElms = document.querySelectorAll('.product-variant');
+let variantsObj = [];
+
+variantElms.forEach(function(variantElm) {
+  variantsObj.push(
+    {
+      id: variantElm.dataset.variantId,
+      inventory_qty: variantElm.dataset.variantInventoryQty
+    })
+});
+
+// console.log(variantElms);
